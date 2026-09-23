790d8929e9b73da25204c3a7f8da7396131b5846
wangzitian0 <wangzitian0@gmail.com>
2026-09-23 20:14:48 +0800

    chore(deps): bump infra2-sdk to v2.1.1 and update submodule (#865) (#867)

--- libs/tests/test_sdk_contract_adoption.py
+++ libs/tests/test_sdk_contract_adoption.py
@@ -17,7 +17,7 @@ OPS_CHECKS = ROOT / ".github/workflows/ops-checks.yml"
 
 
 def test_infra_pins_the_expected_sdk_release() -> None:
-    assert version("infra2-sdk") == "2.1.0"
+    assert version("infra2-sdk") == "2.1.1"
 
 
 def test_alerting_dockerfile_sdk_pin_matches_pyproject() -> None:
